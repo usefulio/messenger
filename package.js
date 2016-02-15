06d65bc472521fae93182c0b577d56a4fb0c2eee
Joshua Ohlman <codewriterohs@gmail.com>
2016-02-15 13:51:31 -0600

    Bump meteor version

--- package.js
+++ package.js
@@ -15,7 +15,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.2.1');
   api.use('useful:mailer', ['client', 'server'], {
     weak: true
   });
